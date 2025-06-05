1592c95dce35476b95e82d1aa44e9812bea28973
Abner Ferreira <83881017+Abner-Ferreira@users.noreply.github.com>
2025-06-05 11:52:57 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from './pages/home'
+import Introduction from './pages/Introduction'
+import Tutorials from './pages/tutorials'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Introduction />
+      <Tutorials />
+    </>
+  )
+}
+
+export default App
